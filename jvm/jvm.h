d690343bb8aaff978cbca398db1eb5a1528f1b16
giangnam0201 <giangnam0201@gmail.com>
2026-08-11 23:35:16 +0700

    Fix: move jvm_value definition before structs that use it

--- jvm/jvm.h
+++ jvm/jvm.h
@@ -35,39 +35,21 @@ typedef double   f8;
 
 enum { T_BOOLEAN=4, T_CHAR=5, T_FLOAT=6, T_DOUBLE=7, T_BYTE=8, T_SHORT=9, T_INT=10, T_LONG=11 };
 
+struct jvm_object;
+typedef union { s4 i; s8 l; f4 f; f8 d; struct jvm_object *r; } jvm_value;
 typedef struct jvm_object { struct jvm_class *cls; u1 *fields; bool is_array; struct { s4 len; u1 *data; u1 etype; } arr; struct jvm_object *next; } jvm_object;
 typedef struct jvm_class {
-    u1 *data; s4 data_len;
-    u2 minor_ver, major_ver;
-    u2 cp_count; u1 *cp_raw;
-    u2 access_flags;
-    u2 this_class;
-    char *name;
-    char *super_name;
+    u1 *data; s4 data_len; u2 minor_ver, major_ver; u2 cp_count; u1 *cp_raw;
+    u2 access_flags; u2 this_class; char *name; char *super_name;
     u2 fields_count; struct jvm_field *fields;
     u2 methods_count; struct jvm_method *methods;
-    s4 static_size; u1 *static_data;
-    s4 inst_size;
-    bool initialized;
-    struct jvm_class *super;
-    s4 index;
+    s4 static_size; u1 *static_data; s4 inst_size;
+    bool initialized; struct jvm_class *super; s4 index;
 } jvm_class;
 typedef struct jvm_field { u2 access; u2 name_idx; u2 desc_idx; s4 offset; jvm_class *cls; } jvm_field;
-typedef struct jvm_method {
-    u2 access; u2 name_idx; u2 desc_idx;
-    u1 *code; s4 code_len; s4 max_stack; s4 max_locals;
-    jvm_class *cls;
-} jvm_method;
-typedef struct jvm_frame { jvm_method *method; u1 *pc; struct jvm_value *locals; struct jvm_value *stack; s4 sp; jvm_object *this_obj; } jvm_frame;
-typedef struct jvm_thread {
-    s4 id;
-    jvm_frame frames[JVM_MAX_FRAMES]; s4 frame_cnt;
-    struct jvm_value stack[JVM_STACK_SIZE];
-    struct jvm_value locals[JVM_STACK_SIZE];
-    bool running;
-    jvm_object *exception;
-} jvm_thread;
-typedef union { s4 i; s8 l; f4 f; f8 d; jvm_object *r; } jvm_value;
+typedef struct jvm_method { u2 access; u2 name_idx; u2 desc_idx; u1 *code; s4 code_len; s4 max_stack; s4 max_locals; jvm_class *cls; } jvm_method;
+typedef struct jvm_frame { jvm_method *method; u1 *pc; jvm_value *locals; jvm_value *stack; s4 sp; jvm_object *this_obj; } jvm_frame;
+typedef struct jvm_thread { s4 id; jvm_frame frames[JVM_MAX_FRAMES]; s4 frame_cnt; jvm_value stack[JVM_STACK_SIZE]; jvm_value locals[JVM_STACK_SIZE]; bool running; jvm_object *exception; } jvm_thread;
 
 extern jvm_thread jvm_threads[JVM_MAX_THREADS];
 extern jvm_thread *jvm_cur;
@@ -84,7 +66,6 @@ void jvm_execute(jvm_thread *t);
 jvm_object *jvm_alloc_obj(jvm_class *c);
 jvm_object *jvm_new_array(jvm_thread *t, s4 cnt, u1 type);
 jvm_object *jvm_new_string_utf8(jvm_thread *t, const char *s);
-jvm_object *jvm_new_string_arr(jvm_thread *t, s4 n, const char **strs);
 void jvm_invoke_static(jvm_thread *t, jvm_class *c, const char *name, const char *desc);
 char *jvm_cp_utf8(jvm_class *c, u2 idx);
 jvm_class *jvm_cp_class(jvm_class *c, u2 idx);
@@ -93,8 +74,6 @@ void jvm_push_l(jvm_thread *t, s8 v);
 void jvm_push_f(jvm_thread *t, f4 v);
 void jvm_push_d(jvm_thread *t, f8 v);
 void jvm_push_r(jvm_thread *t, jvm_object *r);
-void jvm_resolve_methods(jvm_class *c);
-
 void native_method_call(jvm_thread *t, const char *cls, const char *name, const char *desc);
 void bridge_register_natives(void);
 
@@ -104,7 +83,6 @@ extern f8 js_cur_ms(void);
 extern s4  js_resource_exists(const char *p);
 extern s4  js_resource_read(const char *p, void *b, s4 m);
 extern s4  js_resource_size(const char *p);
-
 extern void js_gl_clear(s4 m);
 extern void js_gl_clear_color(f4 r,f4 g,f4 b,f4 a);
 extern void js_gl_viewport(s4 x,s4 y,s4 w,s4 h);
@@ -112,6 +90,7 @@ extern void js_gl_enable(s4 c);
 extern void js_gl_disable(s4 c);
 extern void js_gl_bind_texture(s4 t,s4 x);
 extern void js_gl_gen_textures(s4 n,void *p);
+extern void js_gl_delete_textures(s4 n,void *p);
 extern void js_gl_tex_image_2d(s4 t,s4 l,s4 i,s4 w,s4 h,s4 b,s4 f,s4 ty,void *p);
 extern void js_gl_draw_arrays(s4 m,s4 f,s4 c);
 extern void js_gl_gen_buffers(s4 n,void *p);
@@ -156,7 +135,6 @@ extern void js_gl_fogf(s4 p,f4 v);
 extern void js_gl_fogfv(s4 p,void *v);
 extern void js_gl_generate_mipmap(s4 t);
 extern void js_gl_pixel_storei(s4 p,s4 v);
-extern void js_gl_delete_textures(s4 n,void *p);
 extern void js_gl_shader_source(s4 s,const char *c);
 extern void js_gl_compile_shader(s4 s);
 extern s4  js_gl_create_shader(s4 t);
