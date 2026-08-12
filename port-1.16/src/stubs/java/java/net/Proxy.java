5039d5e1f524580991bfec27478f8463a3df19cd
giangnam0201 <giangnam0201@gmail.com>
2026-08-12 15:07:46 +0700

    Remove accidental Proxy.java commit

--- port-1.16/src/stubs/java/java/net/Proxy.java
+++
@@ -1,7 +0,0 @@
-package java.net;
-public class Proxy {
-    public enum Type { DIRECT, HTTP, SOCKS }
-    public static Proxy NO_PROXY = new Proxy();
-    public Proxy() {}
-}
-class Authenticator {}
