922c482229960b0cd3bb0f74548eb5e43a7f14e3
giangnam0201 <giangnam0201@gmail.com>
2026-08-12 09:27:09 +0700

    Split Gson stub, remove java.net overrides (conflict with TeaVM)

--- port-1.16/src/stubs/java/com/google/gson/GsonBuilder.java
+++ port-1.16/src/stubs/java/com/google/gson/GsonBuilder.java
@@ -4,7 +4,3 @@ public class GsonBuilder {
     public GsonBuilder registerTypeAdapterFactory(Object f) { return this; }
     public Gson create() { return new Gson(); }
 }
-public class Gson {
-    public <T> T fromJson(String j, Class<T> c) { return null; }
-    public String toJson(Object o) { return "{}"; }
-}
--- port-1.16/src/stubs/java/java/net/Authenticator.java
+++
@@ -1,2 +0,0 @@
-package java.net;
-public class Authenticator {}
