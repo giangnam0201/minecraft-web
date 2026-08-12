9cae2054bf9556bebda0a8e5beb007f64e760275
giangnam0201 <giangnam0201@gmail.com>
2026-08-12 09:14:32 +0700

    Fix: package-private stubs, remove java.net (conflicts with java.base)

--- port-1.16/src/stubs/java/java/net/Stubs.java
+++
@@ -1,8 +0,0 @@
-package java.net;
-public class Proxy {
-    public enum Type { DIRECT, HTTP, SOCKS }
-    public static final Proxy NO_PROXY = new Proxy();
-    public Proxy() {}
-    public Proxy(Type t, SocketAddress a) {}
-}
-public class Authenticator {}
