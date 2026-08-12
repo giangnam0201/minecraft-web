acd8e34a95a2e18f72e3dfcc8059ccc0473c125d
giangnam0201 <giangnam0201@gmail.com>
2026-08-12 08:24:55 +0700

    Remove TeaVM annotation import - not on classpath

--- port-1.16/src/wasm/java/net/lax1dude/eaglercraft/v1_16/wasm/Bootstrap.java
+++ port-1.16/src/wasm/java/net/lax1dude/eaglercraft/v1_16/wasm/Bootstrap.java
@@ -1,9 +1,7 @@
 package net.lax1dude.eaglercraft.v1_16.wasm;
 
-import org.teavm.interop.DoNotModifyStaticFields;
 import net.minecraft.client.main.Main;
 
-@DoNotModifyStaticFields
 public class Bootstrap {
     public static void main(String[] args) {
         Main.main(args);
